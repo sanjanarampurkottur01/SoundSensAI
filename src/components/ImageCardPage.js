d1cb5e76a2d7e027f65c1472a301d10e438e01f4
sanjanarampurkottur01 <149339511+sanjanarampurkottur01@users.noreply.github.com>
2024-02-23 10:11:46 -0400

    Update ImageCardPage.js

--- src/components/ImageCardPage.js
+++ src/components/ImageCardPage.js
@@ -6,12 +6,11 @@ function ImageCardPage() {
         { id: 2, emotion: "Anger", description: "Feeling irritated, frustrated, or resentful."},
         { id: 3, emotion: "Fear", description: "Feeling afraid, anxious, or nervous about something."},
         { id: 4, emotion: "Surprise", description: "Feeling astonished, amazed, or taken aback by something unexpected."},
-        { id: 5, emotion: "Disgust", description: "Feeling revulsion or aversion towards something unpleasant or offensive."},
+        { id: 5, emotion: "Sleepy", description: "Feeling sleepy and tired after a long day."},
         { id: 6, emotion: "Love", description: "Feeling affectionate, caring, or deeply attached to someone or something."},
         { id: 7, emotion: "Excitement", description: "Feeling enthusiastic, eager, or thrilled about something upcoming."},
         { id: 8, emotion: "Sadness", description: "Feeling unhappy, sorrowful, or melancholic."},
         { id: 9, emotion: "Calmness", description: "Feeling tranquil, peaceful, or at ease."},
-        { id: 10, emotion: "Confusion", description: " Feeling uncertain, perplexed, or puzzled about something."},
     ]
     return (
         <div>
